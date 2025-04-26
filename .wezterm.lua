8670bb971fcb0e09559b66defc08e56ef102a7bc
Tomek Andrzejewski <andrzejewskit13@gmail.com>
2025-04-26 16:34:30 +0200

    changed sourcing for tpm loading in tmux conifg to make it workk on mac os

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 config.font = wezterm.font("FiraCode Nerd Font")
-config.font_size = 16
+config.font_size = 18
 
 config.enable_tab_bar = false
 
